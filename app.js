7b339dcdaaef49589bd3c6dcd5088975776fbbe4
zhebil <jbilyk1996@gmail.com>
2020-12-03 03:42:12 +0200

    .

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/t", require("./routes/redirect.routes"))
   })
 // }
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
